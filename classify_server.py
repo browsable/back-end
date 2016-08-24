8072e828309276eb600a623977a0616240815c03
browsable <wlsehfdl1>
2016-08-24 21:23:27 +0900

    test

--- classify_server.py
+++ classify_server.py
@@ -1,9 +1,31 @@
 #-*- coding: utf-8 -*-
 
 from sklearn.externals import joblib
-from bottle import route, run, template,request,get, post
-import  time
-from threading import  Condition
+from bottle import route, run, template, request, get, post
+from threading import Condition
+from konlpy.tag import Twitter
+import nltk
+from urllib import quote_plus as q, unquote_plus as unq, urlencode
+from urllib2 import build_opener, urlopen, HTTPCookieProcessor
+from cookielib import CookieJar
+import re
+t = Twitter()
+_CONDITION = Condition()
+
+__author__ = 'bluele'
+
+BASE_URL = 'https://www.google.co.jp'
+BASE_SEARCH_URL = BASE_URL + '/searchbyimage?%s'
+
+REFERER_KEY = 'Referer'
+
+Opener = build_opener(HTTPCookieProcessor(CookieJar()))
+Opener.addheaders = [
+    ('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0'),
+    ('Accept-Language', 'ja,en-us;q=0.7,en;q=0.3')
+]
+
+image_url_pattern = re.compile(ur'^http://www.google.co.jp/imgres\?imgurl=(?P<url>[^&]+)')
 
 
 clf = joblib.load('classify.model')
@@ -14,17 +36,78 @@ joblib.dump(clf,'n_classify.model')
 joblib.dump(cate_dict,'n_cate_dict.dat')
 joblib.dump(vectorizer,'n_vectorizer.dat')
 cate_id_name_dict = dict(map(lambda (k,v):(v,k),cate_dict.items()))
-pred = clf.predict(vectorizer.transform(['[신한카드5%할인][서우한복] 아동한복 여자아동 금나래 (분홍)']))[0]
-print cate_id_name_dict[pred]
 
-_CONDITION = Condition()
+
+
+def set_referer(url):
+    cur = get_referer_index()
+    if cur is not None:
+        del Opener.addheaders[cur]
+    Opener.addheaders.append(
+        (REFERER_KEY, url)
+    )
+
+
+def search_image(url):
+    params = {
+        'image_url': url,
+        'hl': 'ja',
+    }
+    query = BASE_SEARCH_URL % urlencode(params)
+    f = Opener.open(query)
+    url = f.url
+    # domain
+    # url += '&as_sitesearch=zozo.jp'
+    f = Opener.open(url)
+    html = f.read()
+    set_referer(f.url)
+    return html
+
+
+def get_referer_index():
+    i = 0
+    for k, v in Opener.addheaders:
+        if k == REFERER_KEY:
+            return i
+        i += 1
+    else:
+        return None
+
+
+def get_referer():
+    cur = get_referer_index()
+    if cur is not None:
+        return Opener.addheaders[cur]
+    else:
+        return None
+
+
 @route('/classify')
 def classify():
     print "classify called"
-    img = request.GET.get('img','')
+    img = request.GET.get('img', '')
+    # 이미지 사용하지 못함..
+    #     html = search_image(img)
+    #     soup = BeautifulSoup.BeautifulSoup(html)
+    #     cateFromImg = soup.findAll('a', attrs={'class':'_gUb'})
+    #     print cateFromImg
     name = request.GET.get('name', '').decode('utf-8')
+
+    name = re.sub(u'[^가-힣A-Za-z0-9.]+', " ", name)
+    name = re.sub(u'(해외|[가-힣0-20]*?할인[가-힣0-20]*?)', " ", name)
+    name = re.sub(u' +', " ", name)
+    name = re.sub(u' \. ', " ", name)
+    name = re.sub(u' [0-99] ', " ", name)
+    name = " ".join(list(set(x[0] for x in t.pos(name))
+                         .union(x for x in nltk.word_tokenize(name))))
+    names = name.split(" ")
+    name = " ".join(list(set(x for x in names)))
+    name = re.sub(u' +', " ", name)
+    name = re.sub(u' \. ', " ", name)
+    name = re.sub(u'[0-99] ', " ", name)
+
     pred = clf.predict(vectorizer.transform([name]))[0]
-    return {'cate':cate_id_name_dict[pred]}
+    return {'cate': cate_id_name_dict[pred]}
 
 
 run(host='0.0.0.0', port=8887)
--- soma_classifier_72_06.py
+++ soma_classifier_72_06.py
@@ -3,11 +3,13 @@
 import pandas as pd
 from konlpy.tag import Twitter
 from sklearn.feature_extraction.text import CountVectorizer
+from bs4 import BeautifulSoup
 from sklearn.svm import LinearSVC
 from sklearn.grid_search import GridSearchCV
 import numpy as np
 from sklearn.externals import joblib
 import re
+import time
 import requests
 import simplejson
 import json
@@ -18,6 +20,9 @@ from functools import reduce
 from konlpy.tag import Kkma
 from nltk import collocations
 import random
+from selenium import webdriver
+import sys
+
 train_df = pd.read_pickle("soma_goods_train.df")
 train_df.shape
 train_df
@@ -25,14 +30,34 @@ train_df
 k = Kkma()
 t=Twitter()
 vectorizer = CountVectorizer()
-bigram_measures=collocations.BigramAssocMeasures()
 
 
 d_list = []
 cate_list = []
+googlecate_dic = {}
+i = 0
+def searchfile(filePath):
+        browser = webdriver.Firefox()
+        browser.get('http://www.google.com.au/imghp')
+
+        # Click "Search by image" icon
+        elem = browser.find_element_by_class_name('gsst_a')
+        elem.click()
+        # Switch from "Paste image URL" to "Upload an image"
+        browser.execute_script("google.qb.ti(true);return false")
+        # Set the path of the local file and submit
+        ele0 = browser.find_element_by_id("qbfile")
+        ele0.send_keys(filePath)
+        catename = browser.find_element_by_class_name("_gUb").text
+        browser.close()
+        browser.quit()
+        print catename
+        return catename
+
 
 for each in train_df.iterrows():
-
+    time.sleep(2)
+    # 트위터와 nltk를 혼합하여 형태소 추출
     name = re.sub(u'[^가-힣A-Za-z0-9.]+', " ", each[1]['name'])
     name = re.sub(u'(해외|[가-힣0-20]*?할인[가-힣0-20]*?)', " ", name)
     name = re.sub(u' +', " ", name)
@@ -44,7 +69,34 @@ for each in train_df.iterrows():
     name = " ".join(list(set(x for x in names)))
     name = re.sub(u' +', " ", name)
     name = re.sub(u' \. ', " ", name)
-    name = re.sub(u' [0-99] ', " ", name)
+    name = re.sub(u'[0-99] ', " ", name)
+    # n그램 사용
+    # bigram = " ".join(x[0] + x[1] for x in nltk.ngrams(name, 2))
+    # bfinal = ""
+    # for str in bigram.split(" "):
+    #     if len(str) == 1:
+    #         bfinal += "";
+    #     else:
+    #         bfinal += str + " "
+    # bfinal = " ".join(x for x in set(bfinal.split(" ")))
+    # trigram = " ".join(x[0] + x[1] + x[2] for x in nltk.ngrams(name, 3))
+    # tfinal = ""
+    # for str in trigram.split(" "):
+    #     if len(str) == 1:
+    #         tfinal += "";
+    #     else:
+    #         tfinal += str + " "
+    # tfinal = " ".join(x for x in set(tfinal.split(" ")))
+
+    # 구글 이미지 서치 이용하여 이미지의 카테고리 추출, 네임에 태깅
+    # selenium을 사용하기 위해서는 파이어폭스가 설치되어 있어야함
+    filename = each[0]
+    filePath = "/Users/browsable/PycharmProjects/venv/soma_train/"+str(filename)+".jpg"
+    catename = searchfile(filePath)
+    googlecate_dic[filename] = catename
+    name += " "+catename
+    i+=1
+    print i
     print name
     cate = ";".join([each[1]['cate1'], each[1]['cate2'], each[1]['cate3']])
     d_list.append(name)
